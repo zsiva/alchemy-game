d848f7f574680d9911476e96beddccc218dfe7df
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2018-11-29 23:16:13 +0100

    Make ingredients draggable
    Add assets

--- src/components/IngredientsBox/Ingredient.js
+++ src/components/IngredientsBox/Ingredient.js
@@ -1,11 +1,25 @@
 /* @flow */
 import React from 'react';
+import './styles.css';
 
 export type IngredientPropType = {
+  id: string,
   name: string,
   img: string,
+  color: string,
+  onDragStart: () => void,
+  onDragOver: () => void,
+  onDrop: () => void,
 };
 
 export default function Ingredient(props: IngredientPropType) {
-  return <div>{props.name}</div>;
+  return (
+    <div
+      draggable
+      className={`ingredient ${props.name}`}
+      onDragStart={props.onDragStart}
+      onDragOver={props.onDragOver}
+      onDrop={props.onDrop}
+    />
+  );
 }
